d197ecc687048e8214f07dd00e4a1236e41b9ec2
Sanaj <sanaj.jadhav@somaiya.edu>
2024-03-02 18:33:11 +0530

    basic commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const d = new Date();
+let year = d.getFullYear();
+
+function Footer(){
+    return(
+        <footer>
+        <p>Copyright ⓒ {year}</p>
+        </footer>
+    );
+}
+
+export default Footer;
