66751fa4fa4c4f0df52c5dfd81422bd36d9b60f9
Sanaj <sanaj.jadhav@somaiya.edu>
2024-03-29 22:50:47 +0530

    map notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App(){
-    return(<div>
-        <Header></Header>
-        <Note></Note>
-        <Footer></Footer>
-        </div>);
+function App() {
+  return (
+    <div>
+        <Header />
+        {notes.map(note => (
+            <Note
+                key={note.key}
+                title={note.title}
+                content={note.content}
+            />
+        ))}
+        <Footer />
+    </div>
+  );
 }
 
 export default App;
